b73eba371b6c3fe90231f43746e62458e8fae51e
Achal Rajyaguru <45312477+achalrajyaguru@users.noreply.github.com>
2020-12-31 01:51:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//My routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const paymentBRoutes = require("./routes/paymentBRoutes");
+
+//DB Connection
+// mongoose
+//   .connect(process.env.DATABASE, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+//   })
+//   .then(() => {
+//     console.log("DB CONNECTED");
+//   });
+mongoose
+  .connect("mongodb+srv://dbUser:dbUser@tirthcluster.8jifd.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+// const MongoClient = require('mongodb').MongoClient;
+// const URI ="mongodb+srv://dbUser:dbUser@tirthcluster.8jifd.mongodb.net/tshirt?retryWrites=true&w=majority";
+
+// const connectDB = new MongoClient(URI, { useNewUrlParser: true });
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", paymentBRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
---
+++ dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DATABASE:'mongodb+srv://tirth:Shahtirth4@cluster0.wj6am.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
